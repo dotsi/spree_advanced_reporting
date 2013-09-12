7681f19347ea7518f8953847eb2ec8569fa05dde
Miha <miha.kersic@gmail.com>
2013-09-12 09:48:33 +0200

    js fixes

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -4,14 +4,14 @@ Spree::Admin::ReportsController.class_eval do
   before_filter :basic_report_setup, :actions => [:profit, :revenue, :units, :top_products, :top_customers, :geo_revenue, :geo_units, :count]
 
   def add_own
-    return if Spree::Admin::ReportsController::AVAILABLE_REPORTS.has_key?(:geo_profit)
+    return if Spree::Admin::ReportsController::AVAILABLE_REPORTS.has_key?(:top_customers)
     Spree::Admin::ReportsController::AVAILABLE_REPORTS.merge!(ADVANCED_REPORTS)
   end
   I18n.locale = Rails.application.config.i18n.default_locale
   I18n.reload!
 
   ADVANCED_REPORTS ||= {}
-  [ :revenue, :units, :profit, :count, :top_products, :top_customers, :geo_revenue, :geo_units, :geo_profit].each do |x|
+  [ :revenue, :units, :count, :top_products, :top_customers].each do |x|
     ADVANCED_REPORTS[x]= {name: I18n.t("adv_report."+x.to_s), :description => I18n.t("adv_report."+x.to_s)}
   end
 
